7423018c50a4194267f52efc3b2137d8e94e0a09
Stephen Bash <bash@genarts.com>
2014-07-09 11:20:25 -0400

    Minor tweak to baz.c

--- a/baz.c
+++ a/baz.c
@@ -3,6 +3,8 @@ condimentum lectus faucibus. Praesent sodales semper tincidunt. Lorem ipsum
 dolor sit amet, consectetur adipiscing elit. Etiam sit amet dictum nibh, sit
 amet imperdiet tortor. Aenean quis tristique arcu, in sodales nulla. Mauris vel
 euismod libero. Ut eget turpis orci. Vivamus varius lectus et purus adipiscing
-adipiscing. Suspendisse gravida rutrum purus in sollicitudin. In ultrices lectus
+adipiscing. Suspended gravida rutrum purus in sollicitudin. In ultrices lectus
 mauris, in tincidunt sem venenatis eu posuere.
+
+And some more text down here.
 
