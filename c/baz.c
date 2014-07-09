7b999eb3194df89cbd2c36ee39bd7ba0757ea6fe
Stephen Bash <bash@genarts.com>
2014-07-09 11:09:18 -0400

    Initial import of SDK v1

---
+++ c/baz.c
@@ -0,0 +1,8 @@
+Donec nec condimentum tellus. Sed eleifend nisi vel ipsum facilisis, vel
+condimentum lectus faucibus. Praesent sodales semper tincidunt. Lorem ipsum
+dolor sit amet, consectetur adipiscing elit. Etiam sit amet dictum nibh, sit
+amet imperdiet tortor. Aenean quis tristique arcu, in sodales nulla. Mauris vel
+euismod libero. Ut eget turpis orci. Vivamus varius lectus et purus adipiscing
+adipiscing. Suspendisse gravida rutrum purus in sollicitudin. In ultrices lectus
+mauris, in tincidunt sem venenatis eu posuere.
+
